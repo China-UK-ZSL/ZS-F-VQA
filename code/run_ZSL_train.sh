060b6d0fb942aea210ad05a6faa2e6c908cb11d7
陈卓 <zhuochen@chenzhuodeMacBook-Pro.local>
2021-08-10 19:47:09 +0800

    update

--- code/run_ZSL_train.sh
+++ code/run_ZSL_train.sh
@@ -1,3 +1,3 @@
-python main.py --gpu_id 0 --exp_name Zsl_knowledge_space --exp_id W2V --fusion_model SAN --data_choice 3 --method_choice W2V --ZSL 1
-python main.py --gpu_id 0 --exp_name Zsl_semantic_space --exp_id W2V --fusion_model SAN --data_choice 3 --method_choice W2V --ZSL 1 --relation_map 1
-python main.py --gpu_id 0 --exp_name Zsl_object_space --exp_id W2V --fusion_model SAN --data_choice 3 --method_choice W2V --ZSL 1 --fact_map 1
+python main.py --gpu_id 0 --exp_name Zsl_knowledge_space --exp_id W2V --fusion_model SAN --data_choice 3 --method_choice W2V --ZSL 1 --save_model 1
+python main.py --gpu_id 0 --exp_name Zsl_semantic_space --exp_id W2V --fusion_model SAN --data_choice 3 --method_choice W2V --ZSL 1 --save_model 1 --relation_map 1
+python main.py --gpu_id 0 --exp_name Zsl_object_space --exp_id W2V --fusion_model SAN --data_choice 3 --method_choice W2V --ZSL 1 --save_model 1 --fact_map 1
